ea2b63ae32d4d80cc6fb85a9cd6da20fbf5040aa
Chirag Gulati <chirag.gulati282@gmail.com>
2024-05-11 11:57:08 +0530

    token update

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -3,7 +3,7 @@ import {ApiUrl} from './apiConfig';
 const instance = axios.create({
   baseURL: ApiUrl.baseUrl,
   headers: {
-    'X-CMC_PRO_API_KEY': '2144b81d-ac65-4081-84e3-f0cecfad85a2',
+    'X-CMC_PRO_API_KEY': 'PASTE YOUR TOKEN',
   },
 });
 
